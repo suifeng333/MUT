06002050f58d8c5c7fbe898a2f58b6a70acbd322
suifeng7663 <suifeng7663@outlook.com>
2022-07-17 22:00:03 +0800

    首次提交

---
+++ src/main/java/com/suifeng/mut/MutMain.java
@@ -0,0 +1,13 @@
+package com.suifeng.mut;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MutMain.MODID)
+public class MutMain {
+
+    public static final String MODID = "mut";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+}
